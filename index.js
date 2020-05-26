cc14d254cb3a9dbfd562c62db9fb0f6f5c15a9d7
fiyk <fiyk@fiyk.com>
2020-05-26 22:19:27 +0300

    styles

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require("express");
+const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const port = 3000;
 
 server.listen(port, () => {
-  console.log("Server is running on port ${port}");
+  console.log("Server is running on port " + port);
 });
 
 app.get("/", (req, res) => {
