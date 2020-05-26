db949a6b2a13ffee397df8d6fa4cf2e281c0cb9e
fiyk <fiyk@fiyk.com>
2020-05-26 19:23:27 +0300

    express server commits

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require("express");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+server.listen(port, () => {
+  console.log("Server is running on port ${port}");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.emit("message", { manny: "hey how r u?" });
+  socket.on("another event", (data) => {
+    console.log(data);
+  });
+});
