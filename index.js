3833a78c9379efcfe801dd2ae397662ae77acbab
fiyk <fiyk@fiyk.com>
2020-05-27 15:09:21 +0300

    port config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log("Server is running on port " + port);
